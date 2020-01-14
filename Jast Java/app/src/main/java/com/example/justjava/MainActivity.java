71206f8b78af4eec26d10cae927651eccec53c5a
Dima Usatyuk <Shonen1988@gmail.com>
2020-01-14 12:08:07 +0200

    add String variable and massage

--- Jast Java/app/src/main/java/com/example/justjava/MainActivity.java
+++ Jast Java/app/src/main/java/com/example/justjava/MainActivity.java
@@ -25,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMassage = "Total: $" + price + "\nThank you!";
+        displayMessage(priceMassage);
     }
 
     /**
@@ -59,5 +60,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
