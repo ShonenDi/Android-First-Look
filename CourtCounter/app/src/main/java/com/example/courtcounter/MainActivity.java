42e96294b552a97955d3b274425c61af2b92607c
Dima Usatyuk <Shonen1988@gmail.com>
2020-01-19 12:46:05 +0200

    Add team b xml and java code

--- CourtCounter/app/src/main/java/com/example/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
     /**
      * Displays the given scoreTeamA for Team B.
      */
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
+
     /**
      * change value of scoreTeamA variable to team A 3 when put on 3 point button
      */
@@ -58,33 +60,34 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA = 0;
         displayForTeamA(scoreTeamA);
     }
+
     /**
      * change value of scoreTeamB variable to 3 when put on 3 point button
      */
     public void addThreePointTeamB(View view) {
-        scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
      * change value of scoreTeamB variable to 2 when put on 2 point button
      */
     public void addTwoPointTeamB(View view) {
-        scoreTeamA = scoreTeamA + 2;
-        displayForTeamA(scoreTeamA);
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
      * change value of scoreTeamB variable to 1 when put on Free throw button
      */
     public void addOnePointTeamB(View view) {
-        scoreTeamA = scoreTeamA + 1;
-        displayForTeamA(scoreTeamA);
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
     }
 
     public void refreshTeamBPoints(View view) {
-        scoreTeamA = 0;
-        displayForTeamA(scoreTeamA);
+        scoreTeamB = 0;
+        displayForTeamB(scoreTeamB);
     }
 
 
